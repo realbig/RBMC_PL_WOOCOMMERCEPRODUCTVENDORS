b857e80e18da79a70c78b835a394702f37669428
joelworsham <joelworsham@gmail.com>
2015-07-01 11:39:00 -0400

    Version 0.1.7

--- woocommerce-mlm.php
+++ woocommerce-mlm.php
@@ -4,10 +4,10 @@
  * Description: Adds a multi-level-marketing strategy into WooCommerce for reporting purposes.
  * Author: Real Big Marketing
  * Author URI: http://realbigmarketing.com
- * Version: 0.1.6
+ * Version: 0.1.7
  */
 
-define( 'WC_MLM_VERSION', '0.1.6' );
+define( 'WC_MLM_VERSION', '0.1.7' );
 define( 'WC_MLM_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WC_MLM_URL', plugins_url( '', __FILE__ ) );
 
